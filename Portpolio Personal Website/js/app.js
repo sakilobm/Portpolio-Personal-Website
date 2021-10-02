2788fad031986d8aee48fce451953a449bf57537
B.Sakil <59443129+sakilobm@users.noreply.github.com>
2021-10-02 12:30:17 +0530

    Responsive website

---
+++ Portpolio Personal Website/js/app.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+  $(".slider").slick({
+    arrows: false, 
+    dots: true,
+    appendDots: ".slider-dots",
+    dotsClass: "dots",
+  });
+  let hamberger = document.querySelector('.hamberger');
+  let times = document.querySelector('.times');
+
+  hamberger.addEventListener('click', function(){
+
+  })
+  times.addEventListener('click', function(){
+
+  })
+  
+});
